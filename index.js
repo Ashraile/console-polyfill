e4ed15a9f6811799fb0bc6a75e9034c6b422b09c
Ashraile <89170464+Ashraile@users.noreply.github.com>
2025-04-20 21:17:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-// Console-polyfill. MIT license.
-// https://github.com/paulmillr/console-polyfill
-// Make it safe to do console.log() always.
-(function(global) {
-  'use strict';
-  if (!global.console) {
-    global.console = {};
-  }
-  var con = global.console;
-  var prop, method;
-  var dummy = function() {};
-  var properties = ['memory'];
-  var methods = ('assert,clear,count,debug,dir,dirxml,error,exception,group,' +
-     'groupCollapsed,groupEnd,info,log,markTimeline,profile,profiles,profileEnd,' +
-     'show,table,time,timeEnd,timeline,timelineEnd,timeStamp,trace,warn,timeLog,trace').split(',');
-  while (prop = properties.pop()) if (!con[prop]) con[prop] = {};
-  while (method = methods.pop()) if (!con[method]) con[method] = dummy;
-  // Using `this` for web workers & supports Browserify / Webpack.
-})(typeof window === 'undefined' ? this : window);
+// Console-polyfill. MIT license. | https://github.com/paulmillr/console-polyfill | Makes it safe to do console.log() always.
+;(function(w, f, prop, method) { // "use strict";
+    if (!w.console) {w.console = {}}
+    var con = w.console, properties = ['memory'], methods = [
+        'assert|clear|count|debug|dir|dirxml|error|exception|group|groupCollapsed|',
+        'groupEnd|info|log|markTimeline|profile|profiles|profileEnd|show|table|',
+        'time|timeEnd|timeline|timelineEnd|timeStamp|trace|warn|timeLog'
+    ].join('').split('|');
+    while (prop = properties.pop()) { if (!con[prop]) {con[prop] = {}}}
+    while (method = methods.pop()) { if (!con[method]) {con[method] = f}}
+})((typeof window === "undefined" ? this : window), function(){});
